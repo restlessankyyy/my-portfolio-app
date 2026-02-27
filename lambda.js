75a7364fd4db91f1a582cfcfc6424d4d811e0c11
restlessankyyy <rajankit749@gmail.com>
2026-02-27 17:30:59 +0100

    fix(lambda): add binary settings for pdf support

--- lambda.js
+++ lambda.js
@@ -2,5 +2,10 @@
 const serverlessExpress = require('@vendia/serverless-express');
 const app = require('./server');
 
-// Export the Lambda handler
-exports.handler = serverlessExpress({ app });
+exports.handler = serverlessExpress({ 
+  app,
+  binarySettings: {
+    isBinary: ({ headers }) => true,
+    contentTypes: ['application/pdf', 'image/*', 'font/*'],
+  }
+});
